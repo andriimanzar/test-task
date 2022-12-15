e0306b11b0c34c932a0aa8f1034dbf88cbdb0585
andriimanzar <manzar20011@gmail.com>
2022-12-15 23:46:53 +0200

    Add Flyway dependency and migration file

---
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id         BIGSERIAL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    age        int          NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (id)
+)
