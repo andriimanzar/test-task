fea7a8552282df7d2b105215cc4a0ccb69405c76
andriimanzar <manzar20011@gmail.com>
2022-12-15 23:40:30 +0200

    Add UserNotFoundException

--- src/main/java/com/manzar/TestTaskApplication.java
+++ src/main/java/com/manzar/TestTaskApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestTask2Application {
+public class TestTaskApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(TestTask2Application.class, args);
+    SpringApplication.run(TestTaskApplication.class, args);
   }
 
 }
