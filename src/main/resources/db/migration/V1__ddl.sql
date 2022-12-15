20094be4150513a0334dc972a5af0fc9d5729c99
andriimanzar <manzar20011@gmail.com>
2022-12-16 01:09:01 +0200

    Update migration file

--- src/main/resources/db/migration/V1__ddl.sql
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id         BIGSERIAL,
+    id         identity,
     first_name VARCHAR(255) NOT NULL,
     last_name  VARCHAR(255) NOT NULL,
     age        int          NOT NULL,
     CONSTRAINT users_pk PRIMARY KEY (id)
-)
+);
