c24e1ce037a7a48b55544d9fe8cc90aa0b6616e9
andriimanzar <manzar20011@gmail.com>
2022-12-16 00:30:34 +0200

    Add no-args constructor for User class

--- src/main/java/com/manzar/model/User.java
+++ src/main/java/com/manzar/model/User.java
@@ -9,10 +9,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
