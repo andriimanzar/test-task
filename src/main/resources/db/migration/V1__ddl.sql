b6074043919832de6500becef4400f22cc0ed7f6
andriimanzar <andriimanzar@gmail.com>
2022-12-17 13:57:38 +0200

    Add initial data in db

--- src/main/resources/db/migration/V1__ddl.sql
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS users
     birth_date DATE         NOT NULL,
     CONSTRAINT users_pk PRIMARY KEY (id)
 );
+
+INSERT INTO users(first_name, last_name, birth_date)
+VALUES ('First_Name', 'Last_Name',
+        '1999-10-10');
+INSERT INTO users(first_name, last_name, birth_date)
+VALUES ('First_Name#2', 'Last_Name#2',
+        '1990-05-07');
+INSERT INTO users(first_name, last_name, birth_date)
+VALUES ('First_Name#3', 'Last_Name#3', '2003-02-01');
