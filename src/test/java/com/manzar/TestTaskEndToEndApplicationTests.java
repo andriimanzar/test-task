2143c27af4d6f24b6cb534baa1efdf8bb7e1b14c
andriimanzar <andriimanzar@gmail.com>
2022-12-17 13:59:32 +0200

    Update tests

--- src/test/java/com/manzar/TestTaskEndToEndApplicationTests.java
+++ src/test/java/com/manzar/TestTaskEndToEndApplicationTests.java
@@ -6,6 +6,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.manzar.model.User;
 import com.manzar.repository.UserRepository;
+import com.manzar.service.form.UserForm;
+import com.manzar.util.AgeCalculator;
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,22 +27,31 @@ class TestTaskEndToEndApplicationTests {
   private MockMvc mockMvc;
   @Autowired
   private UserRepository userRepository;
-
   @Autowired
   private ObjectMapper objectMapper;
 
   @Test
   void getUserByIdWorkingProperly() throws Exception {
     User testUser = createUser();
-    String resultJson = mockMvc.perform(MockMvcRequestBuilders.get("/api/users/1")).andExpect(
-            status().isOk())
-        .andReturn()
-        .getResponse().getContentAsString();
+    String resultJson = mockMvc.perform(MockMvcRequestBuilders.get("/api/users/1"))
+        .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
 
-    assertEquals(objectMapper.writeValueAsString(testUser), resultJson);
+    UserForm user = objectMapper.readValue(resultJson, UserForm.class);
+
+    assertEquals(testUser.getFirstName(), user.getFirstName());
+    assertEquals(testUser.getLastName(), user.getLastName());
+    assertEquals(AgeCalculator.calculate(testUser.getBirthDate(), LocalDate.now()), user.getAge());
   }
 
+  @Test
+  void getUserByIdWorkingProperlyIfUserNotFound() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/api/users/100")).
+        andExpect(status().isNotFound());
+  }
+
+
   private User createUser() {
-    return userRepository.save(UserFactory.createTestUser());
+    return userRepository.save(
+        new User(1L, "test", "test", LocalDate.of(1995, 5, 15)));
   }
 }
--- src/test/java/com/manzar/service/UserServiceTest.java
+++ src/test/java/com/manzar/service/UserServiceTest.java
@@ -5,13 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.anyLong;
 import static org.mockito.Mockito.when;
 
-import com.manzar.UserFactory;
 import com.manzar.exception.UserNotFoundException;
 import com.manzar.model.User;
 import com.manzar.repository.UserRepository;
+import com.manzar.service.form.UserForm;
+import com.manzar.util.AgeCalculator;
+import java.time.LocalDate;
 import java.util.Optional;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -26,17 +32,29 @@ class UserServiceTest {
   private UserService userService;
 
 
-  @Test
-  void getUserByIdWorkingProperly() {
-    User testUser = UserFactory.createTestUser();
+  @ParameterizedTest
+  @MethodSource("provideUsersAndTheirAge")
+  void getUserByIdWorkingProperly(User testUser) {
     when(userRepository.findById(anyLong()))
         .thenReturn(Optional.of(testUser));
 
-    assertEquals(testUser, userService.getUserById(1L));
+    UserForm user = userService.getUserById(1L);
+
+    assertEquals(testUser.getFirstName(), user.getFirstName());
+    assertEquals(testUser.getLastName(), user.getLastName());
+    assertEquals(AgeCalculator.calculate(testUser.getBirthDate(), LocalDate.now()), user.getAge());
   }
 
   @Test
   void getUserByIdThrowsAnExceptionIfUserNotFound() {
     assertThrows(UserNotFoundException.class, () -> userService.getUserById(1L));
+  }
+
+  private static Stream<Arguments> provideUsersAndTheirAge() {
+    return Stream.of(
+        Arguments.of(new User(1L, "test", "test", LocalDate.of(2000, 10, 10))),
+        Arguments.of(new User(2L, "test", "test", LocalDate.of(2001, 5, 2))),
+        Arguments.of(new User(3L, "test", "test", LocalDate.of(1990, 7, 3))));
+
   }
 }
